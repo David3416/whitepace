1dcedd827d0a08432870224360ed9f656934286f
David3415 <dgagiev@gmail.com>
2026-09-04 15:14:30 +0300

    header-menu WV add html-css

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,26 +1,23 @@
-const products = document.querySelector('.products');
-const productsButton = document.querySelector('.products_button');
-const productsList = document.querySelector('.products_list');
+// const products = document.querySelector('.products');
+// const productsButton = document.querySelector('.products_button');
+// const productsList = document.querySelector('.products_list');
 
-productsButton.addEventListener('mouseenter', () => {
-    productsList.classList.add('products_list_active');
-    productsButton.classList.add('products_button_active');
+// productsButton.addEventListener('mouseenter', () => {
+//     productsList.classList.add('products_list_active');
 
-    productsButton.setAttribute('aria-expanded', true);
-});
+//     productsButton.setAttribute('aria-expanded', true);
+// });
 
-products.addEventListener('mouseleave', () => {
-    productsList.classList.remove('products_list_active');
-    productsButton.classList.remove('products_button_active');
+// products.addEventListener('mouseleave', () => {
+//     productsList.classList.remove('products_list_active');
 
-    productsButton.setAttribute('aria-expanded', false);
-});
+//     productsButton.setAttribute('aria-expanded', false);
+// });
 
-document.addEventListener('click', (event) => {
-    if (!products.contains(event.target)) {
-        productsList.classList.remove('products_list_active');
-        productsButton.classList.remove('products_button_active');
+// document.addEventListener('click', (event) => {
+//     if (!products.contains(event.target)) {
+//         productsList.classList.remove('products_list_active');
 
-        productsButton.setAttribute('aria-expanded', false);
-    }
-});
+//         productsButton.setAttribute('aria-expanded', false);
+//     }
+// });
